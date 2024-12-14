9b16fef566941abfee0056593634b331392071b1
zimrustpro <zimrustpro@gmail.com>
2024-12-14 12:12:23 +0200

    Modify TCP client to send and print message from server

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,5 +1,16 @@
-use std::net::TcpStream;
+use core::str;
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
 
 fn main() {
-    let _stream = TcpStream::connect("localhost:3000").unwrap();
+    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+    let mut buffer = [0; 5];
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Got response from server:{:?}",
+        str::from_utf8(&buffer).unwrap()
+    );
 }
