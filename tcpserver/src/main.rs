a3a097d955e6a2441943a350f68ebf21557f9c03
zimrustpro <zimrustpro@gmail.com>
2024-12-14 12:00:02 +0200

    First iteration of TCP server

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
+    let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    println!("Running on port 3000");
+    for stream in connection_listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection established");
+    }
     println!("Hello, world!");
 }
