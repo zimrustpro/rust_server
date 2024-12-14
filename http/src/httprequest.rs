15316adcf774963615bf19979b956049c0c58e4a
zimrustpro <zimrustpro@gmail.com>
2024-12-14 12:51:31 +0200

    HTTP Version Enum

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -5,6 +5,13 @@ pub enum Method {
     Uninitialized,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized,
+}
+
 impl From<&str> for Method {
     fn from(s: &str) -> Self {
         match s {
@@ -15,12 +22,28 @@ impl From<&str> for Method {
     }
 }
 
+impl From<&str> for Version {
+    fn from(s: &str) -> Self {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_method_into() {
         let m: Method = "GET".into();
         assert_eq!(m, Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let v: Version = "HTTP/1.1".into();
+        assert_eq!(v, Version::V1_1);
     }
 }
